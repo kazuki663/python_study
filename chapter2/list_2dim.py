72a77bfca0b0650daf8ceaf86fc546aca4630f72
kazuki663 <kazkaz.t@icloud.com>
2022-10-02 17:06:53 +0900

    chapter2終了

---
+++ chapter2/list_2dim.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+
+
+data = [
+  ['X-1', 'X-2', 'X-3'],
+  ['Y-1', 'Y-2', 'Y-3'],
+  ['Z-1', 'Z-2', 'Z-3'],
+]
+
+print(data[1][0])
---
+++ chapter2/list_basic.py
@@ -0,0 +1,5 @@
+from dataclasses import dataclass
+
+
+data = ['山田', '佐藤', '田中', '細谷', '鈴木']
+print(data[2])
